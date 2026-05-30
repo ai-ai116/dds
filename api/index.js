03fe640c93e2b253b4ec7c0ea5102cb5fa132af9
amazonprimec <saasifypro@gmail.com>
2026-05-30 19:53:05 +0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -18,7 +18,8 @@ app.get('/', (req, res) => {
 });
 
 // ==================== MongoDB Connection ====================
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/whatsapp-pair';
+const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://erfanx:erfanx@cluster0.k0m6dn2.mongodb.net/?appName=Cluster0/whatsapp-pair
+PORT=3000';
 
 mongoose.connect(MONGO_URI)
   .then(() => console.log('✅ MongoDB Connected'))
